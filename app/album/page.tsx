3072312c893110e6221dea01316e4f88ae9c7193
SeongWoo Shin <ssw4510@gmail.com>
2024-08-24 20:50:38 +0900

    style: update album page's component

--- app/album/page.tsx
+++ app/album/page.tsx
@@ -2,7 +2,7 @@ import Diary from "./_components/Diary";
 
 export default function AlbumPage() {
   return (
-    <div>
+    <div className="flex items-center justify-start w-[340px] h-[812px] mr-1 overflow-hidden">
       <Diary></Diary>
     </div>
   );
