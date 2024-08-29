4ecb4cfd433b315553716051a1f7152078c39ca8
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-08-29 10:14:01 +0900

    ✨ feat: create BottomTabNavigationBar component

--- app/album/page.tsx
+++ app/album/page.tsx
@@ -5,16 +5,20 @@ import { useState } from "react";
 import Diary from "./_components/Diary";
 import Dimmed from "./_components/Dimmed";
 import Share from "./_components/Share";
+import BottomTabNavigationBar from "../_components/BottomTabNavigationBar";
 
 export default function AlbumPage() {
   // 공유하기 컴포넌트 관리하기
   const [isOpenShare, setIsOpenShare] = useState<boolean>(true);
 
   return (
-    <div className="relative flex items-center justify-start w-[375px] h-[812px] mr-1 overflow-hidden">
-      <Diary isOpenShare={isOpenShare} setIsOpenShare={setIsOpenShare} />
-      {!isOpenShare && <Dimmed />}
-      <Share isOpenShare={isOpenShare} setIsOpenShare={setIsOpenShare} />
-    </div>
+    <>
+      <div className="relative flex items-center justify-start w-[375px] h-[812px] mr-1 overflow-hidden">
+        <Diary isOpenShare={isOpenShare} setIsOpenShare={setIsOpenShare} />
+        {!isOpenShare && <Dimmed />}
+        <Share isOpenShare={isOpenShare} setIsOpenShare={setIsOpenShare} />
+      </div>
+      <BottomTabNavigationBar />
+    </>
   );
 }
